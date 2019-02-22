48ae8819b904e8a8352aab5e7d695b20ea7a0ef1
peitl <tomas.peitl@gmail.com>
2019-02-22 11:18:44 +0100

    transformed plotresults.py to use new data structures

--- plotresults.py
+++ plotresults.py
@@ -13,35 +13,28 @@ from operator import itemgetter
 from statistics import pvariance, pstdev, mean
 from collections import defaultdict
 
-classes = []
-configurations = []
 colors = ["r", "g", "b", "y"] * 90
 styles = ["-", "--", ":"] * 120
 
-TIMEOUT=600
-PENALTY=10
-
-def printStats(header, cols):
-    timecol = cols[header.index("Time")]
-    statuscol = cols[header.index("Status")]
-    configcol = cols[header.index("Configuration")]
+def printStats(classes, configurations, instances, rundata, timeout):
     stats = {"ok": 0, "time": 0, "memory": 0, "fault": 0, "signal(9)": 0}
     par10 = 0
     for j, config in enumerate(configurations):
         par10 = 0
         total = 0
         stats["ok"] = stats["time"] = stats["memory"] = stats["fault"] = stats["signal(9)"] = 0
-        for i, status in enumerate(statuscol):
-            if configcol[i] == config:
+        for classname in classes:
+            for instance in instances[classname]:
+                status = rundata[getUID(config, classname, instance)][2]
                 if status not in stats:
                     print("Invalid type of Status on line %d: %s" %(i+2, status))
                     sys.exit(1)
                 stats[status] += 1
                 total += 1
                 if status == "ok":
-                    par10 += float(timecol[i])
+                    par10 += float(rundata[getUID(config, classname, instance)][1])
                 else:
-                    par10 += TIMEOUT*PENALTY
+                    par10 += timeout*10
         print("####################" + "#" * 15 + "##")
         print("# Configuration:    %15s #" % config)
         print("####################" + "#" * 15 + "##")
@@ -55,82 +48,66 @@ def printStats(header, cols):
         print("# PAR10:            %15d #" % (par10/total))
     print("####################" + "#" * 15 + "##")
 
-def cactusPlot(header, cols):
-    timecol = cols[header.index("Time")]
-    statuscol = cols[header.index("Status")]
+def cactusPlot(classes, configurations, instances, rundata, timeout):
     maxLenX = 0
-    for k, Class in enumerate(classes):
-        for j, Configuration in enumerate(configurations):
-            col = [float(x) if statuscol[i] == "ok" else TIMEOUT for i,x in enumerate(timecol) if cols[-2][i] == Class and cols[-1][i] == Configuration]
-            col.sort()
-            X = range(1, len(col) + 1)
+    for j, config in enumerate(configurations):
+        for k, classname in enumerate(classes):
+            times = sorted((rundata[getUID(config, classname, instance)][1] for instance in instances[classname]))
+            X = range(1, len(times) + 1)
             if len(X) > maxLenX:
                 maxLenX = len(X)
-            label = Configuration
-            if Class != "_ALL_":
-                label = Class + "-" + Configuration
-            mp.plot(X, col, colors[k] + styles[j], label=label)
+            label = config
+            if classname != "_ALL_":
+                label = classname + "-" + config
+            mp.plot(X, times, colors[k] + styles[j], label=label)
     mp.legend(loc=0, borderaxespad=0.5)
-    #bbox_to_anchor=(1, 1)
-    mp.axis([0, maxLenX + 1, 0, 2000])
+    mp.axis([0, maxLenX + 1, 0, timeout + 1])
     mp.show()
 
-def analyzeFamilies(header, cols):
-    timecol = cols[header.index("Time")]
-    statuscol = cols[header.index("Status")]
+def analyzeFamilies(classes, configurations, instances, rundata):
     family_data = []
-    for k, Class in enumerate(classes):
-        solved = [None] * len(configurations)
-        for j, Configuration in enumerate(configurations):
-            solved[j] = sum((1 for i in range(len(timecol)) if cols[-2][i] == Class and cols[-1][i] == Configuration and statuscol[i] == "ok"))
+    for classname in classes:
+        solved = [0] * len(configurations)
+        for instance in instances[classname]:
+            for i, config in enumerate(configurations):
+                if rundata[getUID(config, classname, instance)][0] != None:
+                    solved[i] += 1
         mu = mean(solved)
         if mu == 0:
-            print("No solver solved anything from the family '%s'" % Class)
+            print("No solver solved anything from the family '%s'" % classname)
         else:
-            var = pvariance(solved, mu)
             std = pstdev(solved, mu)
             relstd = std / mu
-            family_data.append([Class] + solved + [std, relstd])
-    #family_data.sort(key=itemgetter(-1), reverse=True)
+            family_data.append([classname] + solved + [std, relstd])
     family_data.sort(key=lambda x: x[-2] + x[-1], reverse=True)
+    longest_name = max((len(family[0]) for family in family_data))
+    format_string = "%{}s ".format(longest_name) + "%5d " * len(configurations) + "%7.2f %7.2f"
     print()
     print("Families with the highest discrepancies:")
     print()
     for family in family_data[:20]:
-        print(" ".join(map(str, family)))
+        print(format_string % tuple(family))
 
-def findOutliers(header, cols):
-    timecol = cols[header.index("Time")]
-    statuscol = cols[header.index("Status")]
-    namecol = cols[header.index("Name")]
-    instance_data = defaultdict(list)
-    for j, Configuration in enumerate(configurations):
-        for i,name in enumerate(namecol):
-            if cols[-1][i] == Configuration:
-                instance_data[name + "__" + cols[-2][i]].append(float(timecol[i]))
+def findOutliers(classes, configurations, instances, rundata):
     outliers = []
-    for instance, times in instance_data.items():
-        mu = mean(times)
-        if mu > 0:
-            std = pstdev(times, mu)
-            outliers.append((instance, std))
+    for classname in classes:
+        for instance in instances[classname]:
+            runtime_list = [rundata[getUID(config, classname, instance)][1] for config in configurations]
+            outliers.append((instance, pstdev(runtime_list)))
     outliers.sort(key=itemgetter(1), reverse=True)
-    for x in outliers[:100]:
+    for x in outliers[:20]:
         print(("%s" + " %.02f" * len(instance_data[x[0]])) % (x[0], *instance_data[x[0]]))
 
-def scatterPlot(header, cols):
+def scatterPlot(classes, configurations, instances, rundata):
     assert(len(configurations) == 2)
-    timecol = cols[header.index("Time")]
-    statuscol = cols[header.index("Status")]
-    namecol = cols[header.index("Name")]
-    data = [None, None]
-    for j, Configuration in enumerate(configurations):
-        data[j] = [(namecol[i], max(float(x), 0.00001)) if statuscol[i] == "ok" else (namecol[i], TIMEOUT) for i,x in enumerate(timecol) if cols[-1][i] == Configuration]
-        data[j].sort(key=itemgetter(0))
-        data[j] = [x[1] for x in data[j]]
-    mp.scatter(*data)
+    instance_times = [[], []]
+    for classname in classes:
+        for instance in instances[classname]:
+            for i, config in enumerate(configurations):
+                instance_times[i].append(rundata[getUID(config, classname, instance)][1])
+    mp.scatter(*instance_times)
     mp.plot([0,TIMEOUT*10], [0,TIMEOUT*10])
-    l, u = min(min(data)) / 5, max(max(data)) * 5
+    l, u = min(min(instance_times)) / 5, max(max(instance_times)) * 5
     mp.xlim(l, u)
     mp.ylim(l, u)
     mp.gca().set_aspect('equal', adjustable='box')
@@ -140,20 +117,7 @@ def scatterPlot(header, cols):
     mp.ylabel(configurations[1])
     mp.show()
 
-def readData(filename):
-    global classes, configurations
-    with open (filename, "r") as f:
-        raw = f.readlines()
-    table = [x.replace("\n", "").split(",") for x in raw]
-    header = table[0]
-    cols = list(zip(*(table[1:])))
-    classes = list(set(cols[-2]))
-    configurations = list(set(cols[-1]))
-    classes.sort()
-    configurations.sort()
-    return header, cols
-
-def getEasy(classes, configurations, instances, rundata, but=0, threshold=60):
+def getEasy(classes, configurations, instances, rundata, but=0, threshold=10):
     """
     Returns a list of instances that were solved by at least all but 'but' solvers in uder 'threshold' seconds.
     """
@@ -161,6 +125,8 @@ def getEasy(classes, configurations, instances, rundata, but=0, threshold=60):
     for classname in classes:
         for instance in instances[classname]:
             if sum((ans != None and t <= threshold for (ans, t) in (rundata[getUID(config, classname, instance)] for config in configurations))) + but >= len(configurations):
+                # create a regex pattern for grep filtering
+                #easy.append(instance + ",.*," + classname + ",")
                 easy.append(classname + "/" + instance)
     return easy
 
@@ -197,7 +163,7 @@ def init(filename, aggregate):
 
     classes = set()
     configurations = set()
-    instances = defaultdict(list)
+    instances = defaultdict(set)
     rundata = {}
     timeout = math.inf
 
@@ -220,20 +186,26 @@ def init(filename, aggregate):
             status = linedata[status_idx]
             classname = linedata[class_idx]
             config = linedata[config_idx]
+            
+            if aggregate:
+                instance = classname + "/" + instance
+                classname = "_ALL_"
 
-            classid = "__ALL__" if aggregate else classname
-            classes.add(classid)
+            classes.add(classname)
             configurations.add(config)
 
             if status == "time" and time < timeout:
                 timeout = time
             
-            instances[classid].append(instance)
+            # for logscale
+            time = max(time, 0.001)
+
+            instances[classname].add(instance)
             uid = getUID(config, classname, instance)
             if uid in rundata:
                 print("Error on line %d: duplicate entry for config %s on instance %s of class %s" % (line_number, config, instance, classname))
                 sys.exit(2)
-            rundata[uid] = (answer, time)
+            rundata[uid] = (answer, time, status)
     
     timeout = int(timeout)
     classes = sorted(classes)
@@ -264,17 +236,46 @@ def verify(classes, configurations, instances, rundata):
                 print("The following solvers/configs say False: " + ", ".join(votes[False]))
                 print("The following solvers/configs say True:  " + ", ".join(votes[True]))
 
+    if len(rundata) != len(configurations) * sum((len(instances[classname]) for classname in classes)):
+        success = False
+        print("Error: the number of run-data entries should be number of instances times the number of configurations.")
     return success
+
+def venn(classes, configurations, instances, rundata):
+    """
+    This function calculates the values in a Venn diagram of
+    solved instances by configurations
+    """
+    values = [0] * 2 ** len(configurations)
+    for classname in classes:
+        for instance in instances[classname]:
+            subset_idx = 0
+            for i, config in enumerate(configurations):
+                ans = rundata[getUID(config, classname, instance)][0]
+                if ans != None:
+                    subset_idx += 2 ** i
+            values[subset_idx] += 1
+    for idx, val in enumerate(values):
+        i = 0
+        while idx > 0:
+            if idx % 2 == 1:
+                print(configurations[i] + " ", end="")
+            idx //= 2
+            i += 1
+        print(val)
+
     
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("filename", nargs='?', help="The csv file with the results as parsed by parseresults.py.")
     parser.add_argument("-c", "--cactus", action="store_true", default=False, help="Make a cactus plot of the various configurations and classes.")
+    parser.add_argument("-e", "--easy", type=int, default=0, help="Identify instances solved by all solvers within the given time limit.")
     parser.add_argument("-g", "--aggregate", action="store_true", default=False, help="Treat all classes as one.")
     parser.add_argument("-f", "--families", action="store_true", default=False, help="Find families with large differences between solvers.")
     parser.add_argument("-o", "--outliers", action="store_true", default=False, help="Find instances with large differences between solvers.")
     parser.add_argument("-s", "--stats", action="store_true", default=False, help="Display statistics about the results.")
     parser.add_argument("-t", "--timeout", type=int, default=600, help="Specify the cutoff time that was used for these runs.")
+    parser.add_argument("-v", "--venn", action="store_true", default=False, help="Compute the Venn diagram of solved instances for the configurations.")
     parser.add_argument("-x", "--scatter", action="store_true", default=False, help="Make a scatter plot that compares 2 configurations.")
     args = parser.parse_args()
     
@@ -287,23 +288,23 @@ if __name__ == "__main__":
     classes, configurations, instances, rundata, timeout = init(args.filename, args.aggregate)
     print("Guessed timeout: %d" % timeout)
     if verify(classes, configurations, instances, rundata):
-        print("Solvers agree")
-    threshold = 1
-    print("Number of instances solved by all solvers in under %d seconds: %d" % (threshold, len(getEasy(classes, configurations, instances, rundata, threshold=threshold))))
+        print("Solvers agree ✓")
     
     if args.stats:
-        printStats(header, cols)
+        printStats(classes, configurations, instances, rundata, timeout)
+    elif args.families:
+        analyzeFamilies(classes, configurations, instances, rundata)
+    elif args.outliers:
+        findOutliers(classes, coonfigurations, instances, rundata)
+    elif args.venn:
+        venn(classes, configurations, instances, rundata)
+    elif args.easy > 0:
+        easy_instances = getEasy(classes, configurations, instances, rundata, threshold=args.easy)
+        print("\n".join(easy_instances))
     else:
         import matplotlib.pyplot as mp
-    
-    if args.cactus:
-        cactusPlot(header, cols)
-    
-    if args.scatter:
-        scatterPlot(header, cols)
+        if args.cactus:
+            cactusPlot(classes, configurations, instances, rundata, timeout)
+        elif args.scatter:
+            scatterPlot(classes, configurations, instances, rundata)
 
-    if args.families:
-        analyzeFamilies(header, cols)
-
-    if args.outliers:
-        findOutliers(header, cols)
